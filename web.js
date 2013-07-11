40ba73e1e3e86c81ca7111c3c3a73ad79ce97df3
alvarosp <beholderalv@gmail.com>
2013-07-12 00:24:22 +0200

    Modified web.js

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var dataRead;
+fs.readFile('index.html',function(err, data){
+	dataRead = data;
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	response.send(dataRead);
 });
 
 var port = process.env.PORT || 5000;
